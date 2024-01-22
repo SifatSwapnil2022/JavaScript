9b5fdac545fbd4d5506a24edb65abca3719a7936
MD SIFATULLAH SHEIKH <103960510+SifatSwapnil2022@users.noreply.github.com>
2024-01-22 09:31:48 +0000

    operator conversion

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -15,3 +15,17 @@ let aNumber = 9
 let stringNumber = String(aNumber)
 console.log(stringNumber);
 console.log( typeof stringNumber);
+
+// OPERATIONS*********************
+
+let value = 3
+let negValue = - value
+console.log(negValue)
+
+let stepCounter = 100
+++stepCounter
+console.log(stepCounter)
+
+let pointCounter = 100
+pointCounter++
+console.log(pointCounter)
