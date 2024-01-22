3d6309fe336ed86a239067f7b3875d5f677c20fa
MD SIFATULLAH SHEIKH <103960510+SifatSwapnil2022@users.noreply.github.com>
2024-01-22 08:50:17 +0000

    DataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as new version
+
+//alert("hi sifat") //we are using nodejs , not browser
+
+console.log(3+3)
+
+console.log("sifat")
+
+let name = "SIFAT" // STRING
+let age = 19 //int
+let isLoggedIn = false // boolean
+let state ;
+//symbol = for unique ness 
+//undefined
+
+//null is a object *********
+
+
+
+//object
+console.log(typeof name)
