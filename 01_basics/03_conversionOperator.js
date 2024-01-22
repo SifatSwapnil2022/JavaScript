0431c3df1616d7154f1ad0f3b9f3b66bcacde50c
MD SIFATULLAH SHEIKH <103960510+SifatSwapnil2022@users.noreply.github.com>
2024-01-22 09:09:43 +0000

    Operator_Conversion

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,17 @@
+let score = 33
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+//"33abc" to  convert number  = NaN (not a number)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let aNumber = 9
+let stringNumber = String(aNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
