f5af4bdb5acf95dba3e4fca67bc9c4b5667d75a5
MD SIFATULLAH SHEIKH <103960510+SifatSwapnil2022@users.noreply.github.com>
2024-01-23 14:58:17 +0000

    more_details_objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,9 @@
+const facebookUser = new Object()
+console.log(facebookUser);
+
+const facebookUser1 = {}
+console.log(facebookUser1);
+facebookUser1.id = "2233"
+facebookUser1.name = "sifat"
+facebookUser1.isLoggedIn = false
+console.log(facebookUser1);
