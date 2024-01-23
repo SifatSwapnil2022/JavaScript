cb300768d1b85e3165594c01e209d746df9a1efe
MD SIFATULLAH SHEIKH <103960510+SifatSwapnil2022@users.noreply.github.com>
2024-01-23 17:02:43 +0000

    details_in_objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -7,3 +7,46 @@ facebookUser1.id = "2233"
 facebookUser1.name = "sifat"
 facebookUser1.isLoggedIn = false
 console.log(facebookUser1);
+
+const regularUser = {
+    email: "sp121@gmail.com",
+
+    fullName :{
+        userFullName : {
+    firstName : "sifat",
+    lastName :  "swapnil"
+  }
+ }
+}
+console.log(regularUser.fullName.userFullName);
+
+const obj1 = {
+    1 : "a",
+    2 : "b"
+}
+const obj2 = {
+    3 : "c",
+    4 : "d"
+}
+const obj3 = Object.assign({},obj1,obj2)
+console.log(obj3);
+
+//database
+const users =[ 
+    {
+    id :1,
+    email : "sifat@gamil.com"
+},
+{
+id :1,
+email : "sifat@gamil.com"
+}
+]
+users[1].email
+console.log(facebookUser1);
+console.log(Object.keys(facebookUser1));
+console.log(Object.values(facebookUser1));
+console.log(Object.entries(facebookUser1));
+
+
+console.log(facebookUser1.hasOwnProperty('isLoggedIn'));
