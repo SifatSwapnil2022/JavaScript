218d8590ec14cec910665fe06df7f373fd2b15cb
MD SIFATULLAH SHEIKH <103960510+SifatSwapnil2022@users.noreply.github.com>
2024-01-23 06:40:06 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); //true bcz null as a number ,treating as 0
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+console.log("2" === 2)
+
